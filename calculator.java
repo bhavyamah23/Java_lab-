9aa402e4c08b55c70b7641fa349f0e3b5d427bf3
bhavyamah23 <bhavyamaheshwari.honey@gmail.com>
2026-03-12 22:02:58 +0530

    Add files via upload

--- calculator.java
+++ calculator.java
@@ -19,19 +19,15 @@ public class calculator {
 
         sc.close();
     }
-
     public static void add(int a, int b) {
         System.out.println("Addition: " + (a + b));
     }
-
     public static void sub(int a, int b) {
         System.out.println("Subtraction: " + (a - b));
     }
-
     public static void mul(int a, int b) {
         System.out.println("Multiplication: " + (a * b));
     }
-
     public static void div(int a, int b) {
         if (b == 0) {
             System.out.println("Division: Cannot divide by zero");
